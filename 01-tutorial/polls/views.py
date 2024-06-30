1f7c36a642298b0b28a8632497fade56e71fcc76
uttne <uttne.mkr@gmail.com>
2024-06-30 17:43:47 +0900

    detail と results の中身を実装しDBの操作を行う

--- 01-tutorial/polls/views.py
+++ 01-tutorial/polls/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.db.models import F
+from django.urls import reverse
 
 from .models import Question
 
@@ -19,9 +21,27 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except:
+        return render(
+            request,
+            "polls/detail.html",
+            {"question": question, "error_message": "You didn't select a choice."},
+        )
+    else:
+        # F() は競合回避のために直接フィールドとして値を参照しないようにするために用いる
+        # 結局使うとどうなるかというと SQL の中で計算するように変換がされる
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+
+        # reverse() を使用すると URL をハードコードせずにすむ
+        # html に記述するのと同じように urls の名前と位置引数を与える
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
