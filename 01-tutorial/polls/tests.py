088e9ed15c0924747c26e895d979584a371110ec
uttne <uttne.mkr@gmail.com>
2024-06-30 18:50:31 +0900

    details view に未来のデータが表示されないことを確認するテストの追加 & index view のテスト修正

--- 01-tutorial/polls/tests.py
+++ 01-tutorial/polls/tests.py
@@ -74,8 +74,30 @@ class QuestionIndexViewTests(TestCase):
 
     def test_two_past_questions(self):
         question1 = create_question(question_text="Past question 1.", days=-30)
-        question2 = create_question(question_text="Past question 2.", days=-30)
+        question2 = create_question(question_text="Past question 2.", days=-5)
         response = self.client.get(reverse("polls:index"))
         self.assertQuerySetEqual(
-            response.context["latest_question_list"], [question1, question2]
+            response.context["latest_question_list"], [question2, question1]
         )
+
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        """
+        The detail view of a question with a pub_date in the future
+        returns a 404 not found.
+        """
+        future_question = create_question(question_text="Future question.", days=5)
+        url = reverse("polls:detail", args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """
+        The detail view of a question with a pub_date in the past
+        displays the question's text.
+        """
+        past_question = create_question(question_text="Past Question.", days=-5)
+        url = reverse("polls:detail", args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
