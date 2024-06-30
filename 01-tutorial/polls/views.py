f7197db2cc192a121be85760df1729a791011514
uttne <uttne.mkr@gmail.com>
2024-06-30 16:51:34 +0900

    詳細viewを実装し404の時の実装を追加

--- 01-tutorial/polls/views.py
+++ 01-tutorial/polls/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 
@@ -11,7 +12,10 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    # get_object_or_404 は便利関数
+    # 内部でオブジェクトが存在しないとき(raise Question.DoesNotExist) Http404 を発生させる
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
