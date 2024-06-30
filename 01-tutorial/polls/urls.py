9385bc78471909d165d1006269ba7249af56ed86
uttne <uttne.mkr@gmail.com>
2024-06-30 17:01:33 +0900

    html の中にパスをハードコードしない

--- 01-tutorial/polls/urls.py
+++ 01-tutorial/polls/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     # ex: /polls/
     path("", views.index, name="index"),
     # ex: /polls/5/
-    path("<int:question_id>/", views.detail, name="detail"),
+    # view の html に直接 URL を記述せず url 構文を使うことで urls のパスを変えてもそれに追従してくれる
+    path("specifics/<int:question_id>/", views.detail, name="detail"),
     # ex: /polls/5/results/
     path("<int:question_id>/results/", views.results, name="results"),
     # ex: /polls/5/vote/
